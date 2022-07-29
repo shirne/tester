888f19c746d789d39a3a523c76664009c83740a8
shirne <shirne@126.com>
2022-07-29 10:56:44 +0800

    add

--- lib/scroll_page.dart
+++ lib/scroll_page.dart
@@ -16,6 +16,18 @@ class ScrollPage extends StatelessWidget {
         child: ListView.builder(
           primary: true,
           itemBuilder: (BuildContext context, int index) {
+            if (index == 1) {
+              return Container(
+                margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.blue[200 + index % 4 * 100],
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                height: 100 + index % 4 * 20.0,
+                child: Text('Item: $index'),
+              );
+            }
             return Container(
               margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
               alignment: Alignment.center,
